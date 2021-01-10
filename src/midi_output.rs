b458fe3e5e82b9f469e06e0a0ed039110de0b426
adamnemecek <adamnemecek@gmail.com>
2021-01-09 18:51:41 -0800

    stuff

--- src/midi_output.rs
+++ src/midi_output.rs
@@ -1,12 +1,31 @@
 use crate::prelude::*;
 
+
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub struct MIDIOutput {
+    inner: std::rc::Rc<MIDIOutputImpl>
+}
+
+impl MIDIOutput {
+    fn open(&mut self) {
+        self.inner.open();
+    }
+}
+
+#[derive(Clone, PartialEq, Eq)]
+struct MIDIOutputImpl {
     endpoint: MIDIEndpoint,
     port: Option<coremidi_sys::MIDIPortRef>,
     client: MIDIClient,
 }
 
-impl MIDIOutput {
+impl std::hash::Hash for MIDIOutputImpl {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.endpoint.hash(state);
+    }
+}
+
+impl MIDIOutputImpl {
     fn open(&mut self) {
         self.port = Some(self.client.create_output_port(""));
     }
