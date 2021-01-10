5edc7093f9a6e8b20fe6ddb93603d2b75b337e7a
adamnemecek <adamnemecek@gmail.com>
2021-01-09 18:23:54 -0800

    stuff

--- src/midi_client.rs
+++ src/midi_client.rs
@@ -1,19 +1,38 @@
 use crate::prelude::*;
 
-#[derive(Clone, PartialEq, Eq)]
-struct MIDIClient {
-    inner: coremidi_sys::MIDIClientRef,
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct MIDIClient {
+    inner: std::rc::Rc<MIDIClientImpl>,
 }
 
 impl MIDIClient {
     pub fn new(name: &str) -> Self {
+        Self {
+            inner: std::rc::Rc::new(MIDIClientImpl::new(name)),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Eq)]
+struct MIDIClientImpl {
+    inner: coremidi_sys::MIDIClientRef,
+}
+
+impl std::hash::Hash for MIDIClientImpl {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.inner.hash(state)
+    }
+}
+
+impl MIDIClientImpl {
+    fn new(name: &str) -> Self {
         Self {
             inner: MIDIClientCreate(name),
         }
     }
 }
 
-impl Drop for MIDIClient {
+impl Drop for MIDIClientImpl {
     fn drop(&mut self) {
         unsafe {
             os_assert(coremidi_sys::MIDIClientDispose(self.inner));
